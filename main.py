fc250f1f55bc219ca83faa77ea96689ce5c499a4
Nate <natewbrooks@gmail.com>
2025-12-05 19:51:39 -0500

    Add the current directory to Python path

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the current directory to Python path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi_socketio import SocketManager
 from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
